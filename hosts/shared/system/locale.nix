34f3597fc93265631a3b627d654d5d2c6e50e0e8
Petteri Pucilowski <petteri@hersecret.fi>
2023-02-02 23:14:43 +0700

    change locale to fi_FI

--- hosts/shared/system/locale.nix
+++ hosts/shared/system/locale.nix
@@ -1,19 +1,18 @@
 {lib, ...}: {
   i18n = {
-    defaultLocale = lib.mkDefault "en_US.UTF-8";
+    defaultLocale = lib.mkDefault "fi_FI.UTF-8";
 
     extraLocaleSettings = {
-      LC_TIME = lib.mkDefault "en_US.UTF-8";
+      LC_TIME = lib.mkDefault "fi_FI.UTF-8";
     };
 
     supportedLocales = lib.mkDefault [
-      "en_US.UTF-8/UTF-8"
-      "ja_JP.UTF-8/UTF-8"
+      "fi_FI.UTF-8/UTF-8"
     ];
   };
 
   time = {
-    timeZone = lib.mkDefault "Asia/Jakarta";
+    timeZone = lib.mkDefault "Europe/Helsinki";
     hardwareClockInLocalTime = true;
   };
 }
